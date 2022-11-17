8e9932c19410c1af05d42308b2ea242d567c16d6
ParkJeeHoon <gkstj8300@naver.com>
2022-11-17 11:55:33 +0900

    VueJS 2 Study 완료

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,11 @@
 import Header from './components/layout/Header.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
